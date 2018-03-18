ec565e5edc14ca52053f81060305a9f2fd41cdcf
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-03-18 22:28:37 +0000

    Created menu icons and added them on the main screen

--- app/src/main/java/com/example/c1618639/myapplication/MainActivity.java
+++ app/src/main/java/com/example/c1618639/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.GridLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        mTextMessage = (TextView) findViewById(R.id.message);
+//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
