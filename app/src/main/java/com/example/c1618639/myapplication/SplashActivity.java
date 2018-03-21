5b22634d553366328f12312c404fa7ce75fca01d
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-03-21 22:02:07 +0000

    Animation screen and effect created

--- app/src/main/java/com/example/c1618639/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/c1618639/myapplication/SplashActivity.java
@@ -3,14 +3,22 @@ package com.example.c1618639.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+private TextView tv;
+private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
         setContentView(R.layout.activity_splash);
+        tv = (TextView) findViewById(R.id.tv) ;
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
     }
 }
