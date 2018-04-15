612a3d8537e3629cf5c94aa5aa09b726dd962080
Rahimul Islam Ali <IslamAliRS@cardiff.ac.uk>
2018-04-15 22:30:19 +0100

    minor changes

--- app/src/main/java/com/example/c1618639/myapplication/AboutUsFragment.java
+++ app/src/main/java/com/example/c1618639/myapplication/AboutUsFragment.java
@@ -3,6 +3,7 @@ package com.example.c1618639.myapplication;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +21,19 @@ import android.view.ViewGroup;
 
 public class AboutUsFragment extends Fragment {
 
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_about_us, container, false);
+//    }
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        return v;
     }
 
 //    // TODO: Rename parameter arguments, choose names that match
