17ac3672a87b52ec5fc7c7c9a47aa8b9770238e9
Rahimul Islam Ali <IslamAliRS@cardiff.ac.uk>
2018-04-16 15:02:18 +0100

    about us page now has content from spot-a-bee website.

--- app/src/main/java/com/example/c1618639/myapplication/AboutUsFragment.java
+++ app/src/main/java/com/example/c1618639/myapplication/AboutUsFragment.java
@@ -8,15 +8,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class AboutUsFragment extends Fragment {
+
+    public WebView mWebView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_about_us, container, false);
 
+        mWebView = (WebView) v.findViewById(R.id.webview);
+        mWebView.loadUrl("http://www.spotabee.buzz/about");
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
 
         return v;
     }
