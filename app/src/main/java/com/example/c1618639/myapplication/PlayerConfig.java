c499bd6b2e30be2685784290ba5a74ede6a37f3c
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-04-16 15:34:33 +0100

    Added youtube promo video on media tab, needs adjusting to link it on Main_Fragment

---
+++ app/src/main/java/com/example/c1618639/myapplication/PlayerConfig.java
@@ -0,0 +1,15 @@
+package com.example.c1618639.myapplication;
+
+/**
+ * Created by c1538514 on 16-Apr-18.
+ */
+
+public class PlayerConfig {
+
+    PlayerConfig()
+    {
+
+
+    }
+    public static final String API_KEY = "AIzaSyCiXwUwp1YLX0NlxGGec1vvfnBwlQqjEj4";
+}
