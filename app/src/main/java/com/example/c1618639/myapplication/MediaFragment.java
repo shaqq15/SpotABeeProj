61f6dc5d48212515e60d468b6816a059f44bd14d
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-04-23 14:01:39 +0100

    Youtube API added needs to be linked with MainActivity

--- app/src/main/java/com/example/c1618639/myapplication/MediaFragment.java
+++ app/src/main/java/com/example/c1618639/myapplication/MediaFragment.java
@@ -1,64 +1,65 @@
 package com.example.c1618639.myapplication;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
-import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 
 public class MediaFragment extends Fragment {
-//    implements YouTubePlayer.OnInitializedListener{
-//        public static final String DEVELOPER_KEY = ""
-//        private static final String VIDEO_ID = ""
-//        private static final int RECOVERY_DIALOG_REQUEST = 1;
-//
-//        YouTubePlayerFragment myYoutubePlayerFragment;
-//
-//        @Override
-//        protected void onCreate(Bundle savedInstanceState) {
-//            super.onCreate(savedInstanceState);
-//            setContentView(R.layout.fragment_media);
-//
-//            myYoutubePlayerFragment = (YouTubePlayerFragment)getFragmentManager()
-//                    .findFragmentById(R.id.youtubeplayerfragment);
-//            myYoutubePlayerFragment.initialize(DEVELOPER_KEY, this);
-//        }
-//
-//        @Override
-//        protected void onInitializationFailure(YoutubePlayer.Provider provider,
-//                                                YoutubeInitializationResult errorReason) {
-//            if (errorReason.isUserRecoverableError()) {
-//                errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
-//            } else {
-//                String errorMessage = String.format(
-//                        "Error initializing player",
-//                        errorReason.toString());
-//                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG.show();
-//            }
-//        }
-//        @Override
-//        protected void onInitializationSuccess{YouTubePlayer.Provider provider, YoutubePlayer player,
-//                                                boolean wasRestored) {
-//                    if (!wasRestored) {
-//                        player.cueVideo(VIDEO_ID);
-//                    }
-//            }
-//    }
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_media, container, false);
+    // API
+    private static final String API_KEY = "AIzaSyCiXwUwp1YLX0NlxGGec1vvfnBwlQqjEj4\t";
+
+    // Bee Youtube Video
+    private static String VIDEO_ID = "Fex4B0U43AU";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_media, container, false);
 
 
-        return v;
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+
+
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
+
+
+        youTubePlayerFragment.initialize(API_KEY, new OnInitializedListener() {
+
+
+            @Override
+            public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+                if (!wasRestored) {
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    player.loadVideo(VIDEO_ID);
+                    player.play();
+                }
+            }
+
+
+            @Override
+            public void onInitializationFailure(Provider provider, YouTubeInitializationResult error) {
+                // YouTube error
+                String errorMessage = error.toString();
+                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+                Log.d("errorMessage:", errorMessage);
+            }
+        });
+
+        return rootView;
     }
 }
+
 
 
