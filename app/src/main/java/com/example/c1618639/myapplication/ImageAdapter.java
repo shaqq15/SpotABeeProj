64b1e202483cd7c94bdd44747ad9ca67bfc50567
Luis Toledo <toledol@cardiff.ac.uk>
2018-04-23 16:32:49 +0100

    added functionality to gallery for automatic addition of photos from camera

--- app/src/main/java/com/example/c1618639/myapplication/ImageAdapter.java
+++ app/src/main/java/com/example/c1618639/myapplication/ImageAdapter.java
@@ -1,10 +1,14 @@
 package com.example.c1618639.myapplication;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+
+import java.io.File;
 
 /**
  * Created by c1736285 on 23/04/2018.
@@ -13,20 +17,31 @@ import android.widget.ImageView;
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
 
+    private File[] listOfExternalStorageFiles;
+
     public ImageAdapter(Context c) {
         mContext = c;
+
+        File extStorage = Environment.getExternalStorageDirectory();
+        listOfExternalStorageFiles = extStorage.listFiles();
+
+        //loop over
+
+
+
+
     }
 
     public int getCount() {
-        return mThumbIds.length;
+        return listOfExternalStorageFiles.length;
     }
 
     public Object getItem(int position) {
-        return null;
+        return listOfExternalStorageFiles[position];
     }
 
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     // create a new ImageView for each item referenced by the Adapter
@@ -36,90 +51,92 @@ public class ImageAdapter extends BaseAdapter {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setPadding(8, 8, 8, 8);
         } else {
             imageView = (ImageView) convertView;
         }
 
-        imageView.setImageResource(mThumbIds[position]);
+        //imageView.setImageResource(mThumbIds[position]);
+
+        imageView.setImageURI(Uri.fromFile(listOfExternalStorageFiles[position]));
         return imageView;
     }
 
     // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
-            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//    private Integer[] mThumbIds = {
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
+//            R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background,
 //            R.drawable.sample_4, R.drawable.sample_5,
 //            R.drawable.sample_6, R.drawable.sample_7,
 //            R.drawable.sample_0, R.drawable.sample_1,
@@ -130,5 +147,5 @@ public class ImageAdapter extends BaseAdapter {
 //            R.drawable.sample_2, R.drawable.sample_3,
 //            R.drawable.sample_4, R.drawable.sample_5,
 //            R.drawable.sample_6, R.drawable.sample_7
-    };
+   // };
 }
