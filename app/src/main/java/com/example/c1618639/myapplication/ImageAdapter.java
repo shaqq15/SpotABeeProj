f1601c7c4ad0ea079de958f230c5f493d4c81484
Luis Toledo <toledol@cardiff.ac.uk>
2018-04-23 23:05:08 +0100

    commented out code relying on other segments to avoid crash

--- app/src/main/java/com/example/c1618639/myapplication/ImageAdapter.java
+++ app/src/main/java/com/example/c1618639/myapplication/ImageAdapter.java
@@ -14,54 +14,54 @@ import java.io.File;
  * Created by c1736285 on 23/04/2018.
  */
 
-public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
-
-    private File[] listOfExternalStorageFiles;
-
-    public ImageAdapter(Context c) {
-        mContext = c;
-
-        File extStorage = Environment.getExternalStorageDirectory();
-        listOfExternalStorageFiles = extStorage.listFiles();
+//public class ImageAdapter extends BaseAdapter {
+//    private Context mContext;
+//
+//    private File[] listOfExternalStorageFiles;
+//
+//    public ImageAdapter(Context c) {
+//        mContext = c;
+//
+//        File extStorage = Environment.getExternalStorageDirectory();
+//        listOfExternalStorageFiles = extStorage.listFiles();
 
         //loop over
 
 
 
 
-    }
-
-    public int getCount() {
-        return listOfExternalStorageFiles.length;
-    }
-
-    public Object getItem(int position) {
-        return listOfExternalStorageFiles[position];
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
+//    }
+//
+//    public int getCount() {
+//        return listOfExternalStorageFiles.length;
+//    }
+//
+//    public Object getItem(int position) {
+//        return listOfExternalStorageFiles[position];
+//    }
+//
+//    public long getItemId(int position) {
+//        return position;
+//    }
 
     // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85)); //change this
-            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
-        }
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        ImageView imageView;
+//        if (convertView == null) {
+//            // if it's not recycled, initialize some attributes
+//            imageView = new ImageView(mContext);
+//            imageView.setLayoutParams(new ViewGroup.LayoutParams(85, 85)); //change this
+//            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//            imageView.setPadding(8, 8, 8, 8);
+//        } else {
+//            imageView = (ImageView) convertView;
+//        }
 
-        //imageView.setImageResource(mThumbIds[position]);
+       // //imageView.setImageResource(mThumbIds[position]);
 
-        imageView.setImageURI(Uri.fromFile(listOfExternalStorageFiles[position]));
-        return imageView;
-    }
+//        imageView.setImageURI(Uri.fromFile(listOfExternalStorageFiles[position]));
+//        return imageView;
+//    }
 
     // references to our images
 //    private Integer[] mThumbIds = {
@@ -148,4 +148,4 @@ public class ImageAdapter extends BaseAdapter {
 //            R.drawable.sample_4, R.drawable.sample_5,
 //            R.drawable.sample_6, R.drawable.sample_7
    // };
-}
+//}
