1023a8e009694b5af7a701b45818cc0e6397739d
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-04-23 10:13:29 +0100

    Reduced splash screen transition, added youtube API on media fragment but needs configuration

--- app/src/main/java/com/example/c1618639/myapplication/MediaFragment.java
+++ app/src/main/java/com/example/c1618639/myapplication/MediaFragment.java
@@ -8,11 +8,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 
 
 public class MediaFragment extends Fragment {
-    @Nullable
-    @Override
+//    implements YouTubePlayer.OnInitializedListener{
+//        public static final String DEVELOPER_KEY = ""
+//        private static final String VIDEO_ID = ""
+//        private static final int RECOVERY_DIALOG_REQUEST = 1;
+//
+//        YouTubePlayerFragment myYoutubePlayerFragment;
+//
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.fragment_media);
+//
+//            myYoutubePlayerFragment = (YouTubePlayerFragment)getFragmentManager()
+//                    .findFragmentById(R.id.youtubeplayerfragment);
+//            myYoutubePlayerFragment.initialize(DEVELOPER_KEY, this);
+//        }
+//
+//        @Override
+//        protected void onInitializationFailure(YoutubePlayer.Provider provider,
+//                                                YoutubeInitializationResult errorReason) {
+//            if (errorReason.isUserRecoverableError()) {
+//                errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+//            } else {
+//                String errorMessage = String.format(
+//                        "Error initializing player",
+//                        errorReason.toString());
+//                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG.show();
+//            }
+//        }
+//        @Override
+//        protected void onInitializationSuccess{YouTubePlayer.Provider provider, YoutubePlayer player,
+//                                                boolean wasRestored) {
+//                    if (!wasRestored) {
+//                        player.cueVideo(VIDEO_ID);
+//                    }
+//            }
+//    }
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_media, container, false);
 
@@ -20,3 +60,5 @@ public class MediaFragment extends Fragment {
         return v;
     }
 }
+
+
