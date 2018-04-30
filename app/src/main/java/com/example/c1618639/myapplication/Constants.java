c7b08cf77fc47bc6b7f506a0da6d43316a1645c2
Iasonas Menelaou <MenelaouI@cardiff.ac.uk>
2018-04-30 22:45:38 +0100

    After a failed attempt to merge due to mutiple conflicts, I manually added features from ImageAPI branch into master

---
+++ app/src/main/java/com/example/c1618639/myapplication/Constants.java
@@ -0,0 +1,9 @@
+package com.example.c1618639.myapplication;
+
+/**
+ * Created by c1538514 on 26-Apr-18.
+ */
+
+public class Constants {
+    public static String basic_auth="Basic YWNjX2RhMzcwODQyZGIwZmIzMDphMDQyNGZmN2QzOTIxYTI2ZDRiOWI2OTc5ZDlkOGI1Zg==";
+}
