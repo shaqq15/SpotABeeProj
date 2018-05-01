36ea75107e0c11b8a7e43a524c033573ecaf15f4
Luis Toledo <toledol@cardiff.ac.uk>
2018-05-01 11:29:04 +0100

    added button to change to tutorial gif fragment and changed formatting and text of info page

--- app/src/main/java/com/example/c1618639/myapplication/InfoPageFragment.java
+++ app/src/main/java/com/example/c1618639/myapplication/InfoPageFragment.java
@@ -5,10 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class InfoPageFragment extends Fragment {
@@ -17,6 +18,18 @@ public class InfoPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_info_page, container, false);
 
+
+        Button openTutorial = (Button) v.findViewById(R.id.openTutorial);
+
+        openTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragmentContainer, new tutorial_gif());
+                ft.commit();
+                ft.addToBackStack(null);
+            }
+        });
 
         return v;
     }
